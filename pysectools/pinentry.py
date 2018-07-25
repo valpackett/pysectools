c8d83a41820233085b6a0d478c3df78b002c9b33
Vince Broz <vince@broz.cc>
2018-07-25 09:25:37 -0400

    Remove trailing whitespace

--- pysectools/pinentry.py
+++ pysectools/pinentry.py
@@ -9,7 +9,7 @@ import subprocess
 
 
 def cmd_exists(cmd):
-    return subprocess.call("type " + cmd, shell=True, 
+    return subprocess.call("type " + cmd, shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
 
